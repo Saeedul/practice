9b970eba282cd53473ceaa87157591e82a00b9e9
Saeedul <karimsaeedul476@gmail.com>
2023-05-05 13:21:22 +0600

    discount

--- discount.js
+++ discount.js
@@ -1,6 +1,8 @@
 /*
 1. if ticket numbers is less than 100, per ticket price: 100
 2. if ticket numbers is more than 100, but less than 200. First 100 tickets will be 100/ticket rest tickets will be 90 taka per piece
+first 100 ---> 100tk
+rest ---> 90tk
 3. if you purchase more than 200 tickets
 first 100 --> 100tk
 101-200 ---> 90tk
