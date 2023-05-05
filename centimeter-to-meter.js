9ef78d0d712a451ce49834c794f9ef35bc8e3ff2
Saeedul <karimsaeedul476@gmail.com>
2023-05-05 15:14:19 +0600

    added more

--- centimeter-to-meter.js
+++ centimeter-to-meter.js
@@ -6,5 +6,3 @@ function centimeterToMeter(cm) {
 let cm = 120;
 let m = centimeterToMeter(cm);
 console.log(`${cm} centimeters is equal to ${m} meters.`);
-// const meter = centimeterToMeter(120);
-// console.log(cm, 'is eauql to: ', meter);
