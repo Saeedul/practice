bdaf55eee2d4c5f250af7c65e071663221b61095
Saeedul <karimsaeedul476@gmail.com>
2023-05-06 23:21:50 +0600

    added more

---
+++ biggest-string.js
@@ -0,0 +1,16 @@
+function findBiggestString(array) {
+    let biggestString = '';
+
+    for (let i = 0; i < array.length; i++) {
+        if (typeof array[i] === 'string' && array[i].length > biggestString.length) {
+            biggestString = array[i];
+        }
+    }
+
+    return biggestString;
+}
+
+// Example usage
+let strings = ['sajid', 'mamun', 'kamal', 'jubayer bin rased', 'chinku'];
+let result = findBiggestString(strings);
+console.log('The biggest string:', result);
