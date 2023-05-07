4de1bfccfe9a6e78694d22e59f9eb70286317ace
Saeedul <karimsaeedul476@gmail.com>
2023-05-07 23:40:45 +0600

    added more

--- is-javascript-file.js
+++ is-javascript-file.js
@@ -7,4 +7,4 @@ let file1 = 'script.js';
 let file2 = 'js.png';
 
 console.log(isJavaScriptFile(file1)); // true
-console.log(isJavaScriptFile(file2)); // false
+console.log(isJavaScriptFile(file2)); // fals
