50736563e4464037625f1b85cb40d7280ae5f530
Saeedul <karimsaeedul476@gmail.com>
2023-05-07 17:41:29 +0600

    added more

---
+++ is-javascript-file.js
@@ -0,0 +1,10 @@
+function isJavaScriptFile(fileName) {
+    return fileName.endsWith('.js');
+}
+
+// Example usage
+let file1 = 'script.js';
+let file2 = 'js.png';
+
+console.log(isJavaScriptFile(file1)); // true
+console.log(isJavaScriptFile(file2)); // false
