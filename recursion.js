3216b694abacd098c8873e342c1677f9fa9cd5c7
Saeedul <karimsaeedul476@gmail.com>
2023-05-08 19:21:59 +0600

    added more

---
+++ recursion.js
@@ -0,0 +1,8 @@
+function sum(i) {
+    if (i == 1) { //stopping condition
+        return 1;
+    }
+    return i + sum(i - 1); //calling the function within the function
+}
+const result = sum(5); ///calling the function. When code is executed it will first start from 5 and then move inside the function.
+console.log(result);
